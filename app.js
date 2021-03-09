87ae5746acbf39ba6cf711ac50ec5ed704baf3bd
Max V <maksim.visockis007@gmail.com>
2021-03-09 15:29:24 +0200

    mainiju app.js kodu 13 reizi un ari mainiju kodus index.html un styles.css

--- app.js
+++ app.js
@@ -43,4 +43,35 @@ hideBox.addEventListener('change', function(e){
                                             list.style.display = "initial";
                       }
 });       
-//mainiju 12* reizi
+
+//filter books
+const searchBar = forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', (e) => {
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach((book) => {
+    const title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(e.target.value) != -1){
+      book.style.display = 'block';
+    } else {
+      book.style.display = 'none';
+    }
+  });
+});
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+                      if(e.target.tagName == 'LI'){
+                                            const targetPanel = document.querySelector(e.target.dataset.target);
+                                            Array.from(panels).forEach((panel) => {
+                                                                  if(panel == targetPanel){
+                                                                                        panel.classList.add('active');
+                                                                  }else{
+                                                                                        panel.classList.remove('active');
+                                                                  }
+                                            });
+                      }
+});
+//mainiju 13* reizi
