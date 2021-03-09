c310bc7e126c5c35d4204e2c6ee946213aca0c50
Max V <maksim.visockis007@gmail.com>
2021-03-09 14:57:44 +0200

    mainiju app.js kodu 11 reizi

--- app.js
+++ app.js
@@ -24,11 +24,14 @@ addForm.addEventListener('submit', function(e){
   bookName.textContent = value;
   deleteBtn.textContent = 'delete';
 
+    // add classes
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
     // append to DOM
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
-      //list.insertBefore(li, list.querySelector('li:first-child'));
-      
+
 });
-//mainiju 10* reizi
+//mainiju 11* reizi
