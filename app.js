dcebd681cc2adfdba36689c8d36a1c4ec60caad4
Max V <maksim.visockis007@gmail.com>
2021-03-09 15:12:17 +0200

    mainiju app.js kodu 12 reizi un ari mainiju kodus index.html un styles.css

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ addForm.addEventListener('submit', function(e){
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
-
 });
-//mainiju 11* reizi
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+                      if(hideBox.checked){
+                                            list.style.display = "none";
+                      } else {
+                                            list.style.display = "initial";
+                      }
+});       
+//mainiju 12* reizi
